946aa6d197bea0678dbe519e61f260b276d445a4
Marcin <ozi@post.pl>
2022-10-25 20:22:49 +0200

    add form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [entries, setEntries] = useState([]);
+  const [userName, setUserName] = useState('');
+  const [age, setAge] = useState('');
+
+  const userNameInputChangeHandler = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const ageInputChangeHandler = (e) => {
+    setAge(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    setEntries(...entries, { id: Math.random().toString(), entry: `${userName} (${age} years old)` });
+    setUserName('');
+    setAge('');
+  };
+
+  // console.log(entries);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={formSubmitHandler} className="w-1/2 mx-auto mt-12">
+        <div className="flex flex-col gap-2 p-4 bg-white rounded-2xl">
+          <label className="text-2xl">Username</label>
+          <input type="text" value={userName} onChange={userNameInputChangeHandler} className="border p-2 rounded-md" />
+          <label className="text-2xl">Age (Years)</label>
+          <input type="text" value={age} onChange={ageInputChangeHandler} className="border p-2 rounded-md" />
+          <button type="submit" className="mr-auto mt-2 py-2 px-6 bg-purple-500 text-white rounded-md">
+            Add User
+          </button>
+        </div>
+      </form>
+      <div className="flex flex-col mt-8 gap-2 p-4 bg-white rounded-2xl w-1/2 mx-auto">
+        <div className="border p-2 rounded-md">tetst</div>
+        <div className="border p-2 rounded-md">tetst</div>
+      </div>
+    </>
   );
 }
 
